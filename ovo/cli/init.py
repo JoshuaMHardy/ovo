3cb5bccf23d24c490f7b1bd7d4c9d18d34c5a238
David Prihoda <david.prihoda@msd.com>
2025-11-29 14:15:46 +0100

    Reformat

--- ovo/cli/init.py
+++ ovo/cli/init.py
@@ -188,9 +188,9 @@ All paths can be customized later in the [bold]config.yml[/bold] file.
         console.print("\nYou will need to set the OVO_HOME environment variable to use this home directory.")
         export_command = f'export OVO_HOME="{home_dir}"'
 
-        if (shell_config_path := get_shell_config_path()) and (yes or Confirm.ask(
-            f"\nAdd the OVO_HOME dir to {shell_config_path}?"
-        )):
+        if (shell_config_path := get_shell_config_path()) and (
+            yes or Confirm.ask(f"\nAdd the OVO_HOME dir to {shell_config_path}?")
+        ):
             with open(shell_config_path, "a") as f:
                 f.write(f"\n{export_command}")
             console.print(f"\n✔ Added to {shell_config_path}. Restart your terminal or run:")
